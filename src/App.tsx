22d49be4b485b0e2587a564aa095b2bdb2984309
Jack Einbinder <jackeinbinder01@gmail.com>
2025-01-14 21:38:02 -0500

    assignment1 updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import Labs from "./Labs";      // The App component is the root component that imports other components
-export default function App() { // to break up the HTML rendering task amongst a hierarchy of components
+import Labs from "./Labs";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+export default function App() {
   return (
-    <div>                       // For how we are just rendering the Labs component,
-      <Labs />                  // which for now it's rendering just the Lab1 component
-    </div>
+    <HashRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Navigate to="Labs" />} />
+            <Route path="/Labs/*" element={<Labs />} />
+          </Routes>
+        </div>
+    </HashRouter>
 );}
+
+// The App component is the root component that imports other components
+// to break up the HTML rendering task amongst a hierarchy of components
+// For how we are just rendering the Labs component,
+// which for now it's rendering just the Lab1 component
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,8 +1,26 @@
-import Lab1 from "./Lab1";          // Components can import other components to aggregate the code snippets
-export default function Labs() {    // of the components into larger, more complex HTML content. Here the
-  return (                          // Labs component imports the Lab1 component as the first of a
-    <div id="wd-labs">              // set of exercises that will be implemented in later chapters.
-      <h1>Labs</h1>                 // The Lab1 function is invoked with the HTML syntax <Lab1/>
-      <Lab1 />                      // which is replaced by HTML the function returns implemented in the
-    </div>                          // Lab1 function
+import Lab1 from "./Lab1";
+import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
+import TOC from "./TOC";
+import { Route, Routes, Navigate } from "react-router";
+export default function Labs() {
+  return (
+    <div id="wd-labs">
+      <h1>Labs</h1>
+      <TOC />
+      <Routes>
+        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="Lab1" element={<Lab1 />} />
+        <Route path="Lab2" element={<Lab2 />} />
+        <Route path="Lab3" element={<Lab3 />} />
+      </Routes>
+    </div>
 );}
+
+// Components can import other components to aggregate the code snippets
+// of the components into larger, more complex HTML content. Here the
+// Labs component imports the Lab1 component as the first of a
+// set of exercises that will be implemented in later chapters.
+// The Lab1 function is invoked with the HTML syntax <Lab1/>
+// which is replaced by HTML the function returns implemented in the
+// Lab1 function
