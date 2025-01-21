e78a385a7bc91bd31ba03aa5c70a795865b8cfb0
Jack Einbinder <jackeinbinder01@gmail.com>
2025-01-21 18:01:45 -0500

    updoot

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,6 +4,7 @@ export default function TOC() {
   return (
     <ul>
       <li><Link to="/Labs">Labs</Link></li>
+      <li><a href="https://github.com/jackeinbinder01/kambaz-react-web-app" id="wd-github">My Github Repo</a></li>
       <li><Link to="/Labs/Lab1">Lab 1</Link></li>
       <li><Link to="/Labs/Lab2">Lab 2</Link></li>
       <li><Link to="/Labs/Lab3">Lab 3</Link></li>
